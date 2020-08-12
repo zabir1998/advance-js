350915b9709cce8109d29c7282f09fc61b3fb0e5
zabir1998 <zabirahmed1998@gmail.com>
2020-08-12 20:27:28 +0600

    done

---
+++ arraysplit.js
@@ -0,0 +1,5 @@
+const nums = [1, 2, 3, 4, 5];
+// const part = nums.slice(2, 5);
+const removed = nums.splice(2,2);
+console.log(removed);
+console.log(nums);
---
+++ closure.js
@@ -0,0 +1,13 @@
+function stopWatch(){
+    let count = 0
+    return function(){
+        count++;
+        return count;
+    }
+}
+const clock1 = stopWatch();
+console.log(clock1());
+console.log(clock1());
+console.log(clock1());
+console.log(clock1());
+console.log(clock1());
